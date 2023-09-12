68cf9365eb52db7a5c76cd05e82f75f32f722087
Python4k <python4k@vk.com>
2023-09-13 01:55:13 +0300

    add a separate page for each blog

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -8,4 +8,11 @@ class PostView(View):
     def get(self, request):
         posts = Post.objects.all()
         return render(request, 'blog/blog.html', {'post_list': posts})
+    
+    
+class PostDetail(View):
+    '''Отдельная страница записи'''
+    def get(self, request, pk):
+        post = Post.objects.get(id=pk)
+        return render(request, 'blog/blog_detail.html', {'post': post})
         
